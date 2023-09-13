177fde3968097e89801a4ca2425bb994fa46994b
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-09-12 22:35:55 -0300

    aula 12-09-23

---
+++ src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './header.css';
+
+function Header() {
+  return (
+    <header>
+      <div >
+        <nav className='navbar'>
+          Início
+          Cadastro
+          Pesquisa
+          Login
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Header from '../../header/header';
+import Footer from '../../footer/footer';
+
 
 function Home() {
   return (
     <div >
+      <Header/>
       Home do Site
+      <Footer/>
     </div>
   );
 }
