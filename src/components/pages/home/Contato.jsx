01239091f0ae1f2f43a49116c114d061f1ad7611
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-09-21 22:37:23 -0300

    aula 21-09-23

--- src/components/pages/home/Contato.jsx
+++ src/components/pages/home/Contato.jsx
@@ -2,20 +2,35 @@ import React from 'react';
 import Header from '../../header/header';
 import Footer from '../../footer/footer';
 
+import './Contato.css';
+
 
 function Contato() {
   return (
     <div >
       <Header pag={'Contato'}/>
-        <p
-          style={{
-            margin: '10rem',
-            textAlign: 'center',
-            fontSize: '3rem'
-          }}
-        >
-        Contato
-        </p>
+
+        <div className='contatos'>
+          <h1 className='contatoH1'>Contatos</h1>
+          <p className='contatoP'>
+            Use nosso formulário de contato para todas as solicitações de informações ou entre em contato conosco diretamente usando as informações de contato abaixo. Sinta-se à vontade para entrar em contato conosco via e-mail ou telefone
+          </p>
+          <div className='contatoBox'>
+            <div className='contatoIcons'>
+              <div className='contatoLocal'>
+                <span></span>
+                <h3></h3>
+                <p></p>
+              </div>
+              <div className='contatoTel'>
+                <span></span>
+                <h3></h3>
+                <p></p>
+              </div>
+            </div>
+          </div>
+        </div>
+
       <Footer/>
     </div>
   );
--- src/components/pages/usuarios/Login.jsx
+++ src/components/pages/usuarios/Login.jsx
@@ -1,23 +1,92 @@
-import React from 'react';
+/*import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { MdLogin } from "react-icons/md";
+
 import Header from '../../header/header';
 import Footer from '../../footer/footer';
 
+import api from '../../services/api';
+
+import './Login.css'
+
 function Login() {
+
+  let navigate = useNavigate();
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function handleSubmit(event){
+    event.preventDefault();
+    logar();
+  }
+
+  async function logar(event){
+
+    try {
+      const dados = {
+        login,
+        senha
+      }
+      const response = await api.post('/usuarios/login', dados);
+
+      if (response.data.confirma == true){
+        const objLogado = {
+          "id": response.data.id,
+          "nome": response.data.nome,
+          "acesso": response.data.tipo
+        };
+
+        localStorage.clear();
+        localStorage.setItem('user', JSON.stringify(objLogado));
+        navigate('/');
+      } else {
+        alert('Erro: ' + response.data.message)
+      }
+    } catch (error){
+      if (error.response){
+        alert(error.response.data.message)
+      } else {
+        alert(error);
+      }
+    }
+  }
+
   return (
-    <div >
-      <Header pag={'Login'} />
-        <p
-          style={{
-            margin: '10rem',
-            textAlign: 'center',
-            fontSize: '3rem'
-          }}
-        >
-        Login
-        </p>
+    <>
+      <Header pag={'login'} />
+
+        <div class="containerLog">
+          <div>
+            <h2>Acessar o site</h2>
+          </div>
+            <form id="form" class="form" onSubmit={handleSubmit}>
+              <input
+                type="text"
+                id="email"
+                placeholder="E-mail"
+                onChange={v => setLogin(v.target.value)}
+                value={login}
+              />
+              <input
+                type="password"
+                id="password"
+                placeholder="Senha"
+                onChange={v => setSenha(v.target.value)}
+                value={senha}
+              />
+              <div class="info">
+                <Link to='/cadusuarios'>Não tenho cadastro!</Link>
+                <a href="#">Esqueci o e-mail</a>
+              </div>
+              <button type="submit" className='botao'><MdLogin className='ico' /> Entrar</button>
+            </form>
+        </div>
+
       <Footer />
-    </div>
+    </>
   );
 }
 
 export default Login;
+*/
