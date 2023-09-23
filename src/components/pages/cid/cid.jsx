d2d50813bb58936322068995badb1c114001d3a2
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-09-22 22:36:01 -0300

    aula 22-09-2023

--- src/components/pages/cid/cid.jsx
+++ src/components/pages/cid/cid.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../header/header';
 import Footer from '../../footer/footer';
 
-function Cid() {
+function Cid({ Cid }) {
+
+  const [cid, setCid] = useState (
+    [
+      {
+        "cid_id": 1,
+        "cid_cid": "A01",
+        "cid_descri": "Febre tifoide"
+      }
+    ]
+  );
+
   return (
     <div >
       <Header pag={'Cid'}/>
@@ -15,6 +26,12 @@ function Cid() {
         >
         Tela de CID
         </p>
+
+        <div key={cid.cid_id}>
+          <span>{cid.cid_cid}</span>
+          <span>{cid.cid_descri}</span>
+        </div>
+
       <Footer/>
     </div>
   );
--- src/components/pages/endereço/endereco.jsx
+++ src/components/pages/endereço/endereco.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../../header/header';
 import Footer from '../../footer/footer';
 
-function Endereco() {
+function Endereco({ Endereco }) {
+
+  //parei aqui
+  const [endereco, setEndereco] = useState (
+    [
+      {
+        "enderec_id": 1,
+        "enderec_rua": "Rua das Flores",
+        "enderec_num": 123,
+        "enderec_bairro": "Bairro X",
+        "enderec_complem": "Apto 101",
+        "enderec_cidade": "Tupa",
+        "enderec_cep": "12345678",
+        "enderec_estado": "SP",
+        "pct_pront_enderec": 1
+      }
+    ]
+  );
+
   return (
     <div >
       <Header pag={'Endereco'}/>
