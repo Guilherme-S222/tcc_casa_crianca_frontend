319617cd3b73c5d13f12cbfe0adab49d4b700907
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-11-24 22:36:20 -0300

    login-ok

--- src/components/pages/usuarios/Login.jsx
+++ src/components/pages/usuarios/Login.jsx
@@ -5,47 +5,52 @@ import { MdLogin } from "react-icons/md";
 import Header from '../../header/header';
 import Footer from '../../footer/footer';
 
+import api from '../../services/api';
+
 import './Login.css';
 
 function LoginUsu() {
 
-  // let navigate = useNavigate();
-  // const [login, setLogin] = useState('');
-  // const [senha, setSenha] = useState('');
+  let navigate = useNavigate();
+  const [login, setLogin] = useState('');
+  const [senha, setSenha] = useState('');
 
-  // function handleSubmit(event){
-  //   event.preventDefault();
-  //   logar();
-  // }
+  function handleSubmit(event){
+    event.preventDefault();
+    logar();
+  }
 
-  // async function logar(event){
-  //   try{
-  //     const dados = {
-  //       login,
-  //       senha
-  //     }
-  //     const response = await api.post('/usuarios/Login', dados);
+  async function logar(event){
+    try{
+      const dados = {
+        login,
+        senha
+      }
+      const response = await api.post('/login', dados);
 
-  //     if (response.data.confirma == true){
-  //       const objLogado = {
-  //         "id": response.data.id,
-  //         "nome": response.data.nome,
-  //         "acesso": response.data.tipo
-  //       };
-  //       localStorage.clear();
-  //       localStorage.setItem('user', JSON.stringify(objLogado));
-  //       navigate('/');
-  //     } else {
-  //       alert('Erro: ' + response.data.message)
-  //     }
-  //   } catch(error){
-  //     if (error.response){
-  //       alert(error.response.data.message);
-  //     } else {
-  //       alert(error);
-  //     }
-  //   }
-  // }
+      if (response.data.confirma === true){
+        const objLogado = {
+          "id": response.data.id,
+          "nome": response.data.nome,
+          "acesso": response.data.tipo
+        };
+
+        localStorage.clear();
+        localStorage.setItem('user', JSON.stringify(objLogado));
+        navigate('/menu');
+
+      } else {
+        alert('Erro: ' + response.data.message)
+      }
+
+    } catch(error){
+      if (error.response){
+        alert(error.response.data.message);
+      } else {
+        alert(error);
+      }
+    }
+  }
 
   return(
     <>
@@ -54,25 +59,25 @@ function LoginUsu() {
         <div>
           <h2>Acessar o login</h2>
         </div>
-        <form id='form' className='form' autoComplete='off' /*</div>onSubmit={handleSubmit}*/>
+        <form id='form' className='form' autoComplete='off' onSubmit={handleSubmit}>
           <input
             type= "text"
-            id= "email"
-            placeholder= "E-mail"
-            /*onChange={v => setLogin(v.target.value)}*/
-            /*value={login}*/
+            id= "login"
+            placeholder= "Usuário"
+            onChange={v => setLogin(v.target.value)}
+            value={login}
           />
           <input
             type= "password"
             id= "password"
             placeholder= "Senha"
-            /*onChange={v => setSenha(v.target.value)}*/
-            /*value={senha}*/
+            onChange={v => setSenha(v.target.value)}
+            value={senha}
           />
           <div className='info'>
             <Link to='/CadUsuarios'>Não tenho cadastro</Link>
             <Link to='#'>Esqueci o email</Link>
-            <Link to='../menu'>*/</Link>
+            <Link to='../menu'>*</Link>
           </div>
           <button type="submit" className='botao'><MdLogin className='ico' />Entrar</button>
         </form>
