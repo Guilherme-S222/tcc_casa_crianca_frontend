29a670bd1a2914b23490ae9415e024ef8369ba5c
Guilherme-S222 <guilherme.anjolette@etec.sp.gov.br>
2023-11-28 01:11:57 -0300

    Comments api - urls

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,8 +1,12 @@
 import axios from 'axios'; //Cliente HTTP baseado em promessas para o navegador e Node.js
 
 const api = axios.create({
-  //baseURL: 'http://10.67.23.36:3333'
-  baseURL: 'http://localhost:3333'
+
+  //baseURL: 'http://10.67.23.36:3333' // etec server
+
+  //baseURL: 'http://10.67.23.52:3333' // etec server maquina 18
+  
+  baseURL: 'http://localhost:3333' // local server
 });
 
 export default api;
